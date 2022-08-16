3d74ce979794f56a760b8b1d6a9ab2365576756e
MickKorogod <korogod1995@gmail.com>
2022-08-17 00:34:00 +0300

    tskcmt

--- src/index.js
+++ src/index.js
@@ -1,26 +1,28 @@
 import './css/styles.css';
 import { Notify } from 'notiflix';
 import debounce from 'lodash.debounce';
-import {fetchCountries} from './fetchCountries'
+import {fetchCountries }from './fetchCountries'
 import { 
     renderMarkupCountryList,
     renderMarkupCountryInfo,
 } from './markup'
-const input = document.querySelector('input')
+const inputCountry = document.querySelector('input')
 const countryList = document.querySelector('.country-list')
 const countryInfo = document.querySelector ('.country-info')
 
 const DEBOUNCE_DELAY = 300;
 const limit = 10;
+inputCountry.addEventListener('input', debounce(onCountrySearch, DEBOUNCE_DELAY));
 
-input.addEventListner('input', debounce( onCountrySearch, DEBOUNCE_DELAY));
 function onCountrySearch(e) {
+
     e.preventDefault()
-    const countryName = input.value
+
+    const countryName = inputCountry.value
     const name = countryName.trim();
     if (name === '') {
         clearMarkup();
-        return;
+        return;     
     }
 
     fetchCountries(name)
@@ -41,14 +43,39 @@ function showCountryCard(countries) {
         Notify.info('Too many matches found. Please enter a more specific name.')
         return
     }
-    else if ( countries.length === 1 ) {
+    else if (countries.length === 1) {
+        countryList.innerHTML = ''
         countryInfo.innerHTML = renderMarkupCountryInfo(countries[0])
     }
     else {
         let countryListMarkup = '';
         countries.map(country => {
             countryListMarkup += renderMarkupCountryList(country);
+            countryInfo.innerHTML = ''
         });
-        countryList.insertAjecenHTML('beforeend', countryListMarkup);
+        countryList.insertAdjacentHTML('beforeend', countryListMarkup);
     }
 }
+
+//     API.fetchCountries(countryName)
+//         .then((data) => insertContent(data))
+//         .catch(error => {
+//         console.log(error)
+//     })
+// }
+
+// const creatListItem = (item) =>
+//    `<li>
+//   <img src="${item.flags.svg}" alt="${item.name.official}" width = "240" height = "160">
+//   <h2>${item.name.official ? item.name.official: ""}</h2>
+//   <p>Population:${item.population ? item.population: ""}</p>
+//   <p>Capital:${item.capital ? item.capital: ""}</p>
+  
+// </li>`
+
+// const generateContent = (array) => array?.reduce((acc, item) => acc + creatListItem(item))
+// const insertContent = (array) => {
+// const res = generateContent(array);
+// countryInfo.insertAdjacentHTML('beforeend',res)
+// }
+     
--- src/markup.js
+++ src/markup.js
@@ -1,34 +1,54 @@
-export function renderMarkupCountryInfo ( {
+
+export function renderMarkupCountryInfo({
     name,
     capital,
     population,
     langueges,
-    flags,
+    flags,  
 
 }) {
+    return`<li>
+ <img src="${flags.svg}" alt="${name.official}" width = "240" height = "140">
+<h2>${name.official ? name.official : ""}</h2>
+<p>Population:&nbsp${population ? population : ""}</p>
+<p>Capital:&nbsp${capital ? capital : ""}</p>
+<p>Langueges:&nbsp ${{ langueges }}</p>
+
+</li>`}
+
+export function renderMarkupCountryList({ name, flags, }) {
     return `
-    <div class = "country-card">
-<h2 class="country-name">${name.common}</h2>
-<img src = ${flags.svg} class="country-flag" width = "240" height = "160">
-<ul class = "country-list">
-<li class = "country-item">
-<h3>Capital:&nbsp</h3>
-<p>${capital}</p>
-</li>
-<li class = "country-item">
-<h3>Population:&nbsp</h3>
-<p>${population}</p>
-</li>
-<li class = "country-item">
-<h3>Langueges:&nbsp</h3>
-<p>${Object.values(langueges)}</p>
-</li>`;
+   <ul> <li class="country-item">
+    <img src = "${flags.svg}" alt ="country-flag" width = "20" height = "20">
+    <p class = "counrty-name"><b>${name.common}</b></p>
+    </li></ul>`
 }
 
-export function renderMarkupCountryList({ name, flags }) {
-    return `
-    <li class="country-item">
-    <img src = "${flags.svg}" alt ="country-flag" width = "20" height = "20">
-    <p><b>${name.common}</b></p>
-    </li>`
-}
+// export function renderMarkupCountryInfo ( {
+//     name,
+//     capital,
+//     population,
+//     langueges,
+//     flags,
+
+// }) {
+//     return `
+//     <div class = "country-card">
+// <h2 class="country-name">${name.common}</h2>
+// <img src = ${flags.svg} class="country-flag" width = "240" height = "160">
+// <ul class = "country-list">
+// <li class = "country-item">
+// <h3>Capital:&nbsp</h3>
+// <p>${capital}</p>
+// </li>
+// <li class = "country-item">
+// <h3>Population:&nbsp</h3>
+// <p>${population}</p>
+// </li>
+// <li class = "country-item">
+// <h3>Langueges:&nbsp</h3>
+// <p>${Object.values(langueges)}</p>
+// </li>
+// </ul>
+// </div>`;
+// }
